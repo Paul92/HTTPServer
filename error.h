e71b8f94295145ba17c49d9cf8bf808dd36cf180
Paul92 <paul.gafton@gmail.com>
2013-08-19 20:42:33 +0300

    Added basic error library

---
+++ error.h
@@ -0,0 +1,19 @@
+//SERVER ERRORS
+void errorOnBinding();
+void errorOnAccept();
+
+//CLIENT ERRORS
+void errorWritingOnSocket();
+void errorConnecting();
+void noServerFound();
+
+//COMMON ERRORS
+void errorOpeningSocket();
+void errorOpeningFile();
+
+//I/O ERRORS
+void errorReadingFromFile();
+void errorReadingFromSocket();
+void errorBadFileName();
+
+void errorOnFork();
