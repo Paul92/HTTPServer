cba3ed09185d2a1618ab91dd89ca1e51f6167ca0
Paul92 <paul.gafton@gmail.com>
2013-10-05 16:03:01 +0300

    Php get dynamic request

--- dynamic.c
+++ dynamic.c
@@ -2,43 +2,51 @@
 #include <string.h>
 #include <stdlib.h>
 #include "structs.h"
+#include "static.h"
 #include "dynamic.h"
-
-#define LOCATION "localhost"
-#define RESPONSE_SIZE 1024
-#define INF 214748364
-
-char *patrat(char* uri){
-
-    while(*uri!='?')
-        uri++;
-
-    uri++;  // to remove '?' too
-
-    char *response = malloc(RESPONSE_SIZE);
-
-    int val = INF;
-    int ret = sscanf(uri, "a=%d", &val);
-
-    if(ret == 0 || val == INF)
-        strcpy(response, "<html>A avut loc o eroare</html>");
-    else
-        sprintf(response, "<html>Patratul lui %d este %d</html>", val, val*val);
-
-    return response;
-}
-
+#include "constants.h"
 
 void dynamicHandler(struct headers *request, int sockfd){
     struct responseHeaders headers = {200, "OK", LOCATION, "text/html", "UTF-8", 0};
     char *response;
-    char *uri = getHeader(request, "URI");
-    if(strncmp(uri, "/patrat", 7) == 0){
-        response = patrat(uri);
+    char *uri = getHeader(request, "URI:");
+
+    char filePath[MAX_URI_SIZE] = DOC_ROOT;
+    strcat(filePath, uri);
+
+    char *end = strchr(filePath, '?');
+    if(end) *end = '\0';
+
+    char* bias = strchr(uri, '?');
+    if(bias) bias++;
+
+    printf("%s\n", bias);
+    char* space;
+    while(bias && (space = strchr(bias, '&'))){
+        *space = ' ';
     }
-    headers.fileSize = strlen(response);
+
+    printf("%s\n", bias);
+    printf("CMD\n");
+
+    char cmd[MAX_CLI_COMMAND] = "php-cgi -f ";
+    strcat(cmd, filePath);
+    strcat(cmd, " ");
+    strcat(cmd, bias);
+
+    printf("CMD: %s", cmd);
+
+    FILE *f = popen(cmd, "r");
+    if(f != NULL){
+        fseek(f, 0, SEEK_END);
+        headers.fileSize = ftell(f);
+        fseek(f, 0, SEEK_SET);
+    }
+
     sendHeaders(headers, sockfd);
-    write(sockfd, response, headers.fileSize);
+
+    sendFile(f, sockfd);
+
 
 }
 
